67146443c0084dd0943da1f5e438484f6770b28d
Dutkercz <dutkercz@gmail.com>
2025-01-18 07:02:10 -0300

    First commit local

---
+++ Dev-2025-Project-Mongo/src/main/java/Dev_2025/Dev_2025_Project_Mongo/Dev2025ProjectMongoApplication.java
@@ -0,0 +1,13 @@
+package Dev_2025.Dev_2025_Project_Mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev2025ProjectMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dev2025ProjectMongoApplication.class, args);
+	}
+
+}
---
+++ Dev-2025-Project-Mongo/src/test/java/Dev_2025/Dev_2025_Project_Mongo/Dev2025ProjectMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package Dev_2025.Dev_2025_Project_Mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev2025ProjectMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
